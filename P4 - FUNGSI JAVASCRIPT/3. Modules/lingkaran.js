23e8478ab533802d87cb06a09508f01635d440d1
arientaulia <161545871+arientauliaa@users.noreply.github.com>
2024-10-09 15:21:12 +0700

    Add files via upload

---
+++ P4 - FUNGSI JAVASCRIPT/3. Modules/lingkaran.js
@@ -0,0 +1,3 @@
+import {PHI} from './math-constant.js';
+export const luasLingkaran = (radius) => PHI * radius ** 2;
+export const kelilingLingkaran = (radius) => 2 * PHI * radius;
---
+++ P4 - FUNGSI JAVASCRIPT/4. Export Default/bola.js
@@ -0,0 +1,3 @@
+import {PHI} from '../3. Modules/math-constant.js';
+const volume = (radius) => 4 * PHI * radius ** 3;
+export default volume;
